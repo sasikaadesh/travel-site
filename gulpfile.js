5d5daa67454fa1858e2b3ee69b84c3e360c7c83e
Sasika Rodrigo <rodrsas@cacorp.com>
2019-03-22 17:15:21 +0530

    Completed large hero block and intro content block

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+// var gulp = require('gulp'),
+// watch = require('gulp-watch'),
+
+// gulp.task('default', function() {
+// 	console.log("Hooray you");
+// });
+
+// gulp.task('html', function() {
+// 	console.log("html is here");
+// });
+
+require('./gulp/tasks/styles');
+require('./gulp/tasks/watch');
